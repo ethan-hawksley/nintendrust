338c5ab8c718e1ddf000aae1c401c1b171d998b4
ethan-hawksley <136483392+ethan-hawksley@users.noreply.github.com>
2026-01-25 19:36:06 +0000

    add ROM struct enhancements with CHR ROM, mapper, and screen mirroring support

--- src/rom.rs
+++ src/rom.rs
@@ -1,6 +1,15 @@
+pub enum Mirroring {
+    Vertical,
+    Horizontal,
+    FourScreen,
+}
+
 pub struct Rom {
     pub header: [u8; 16],
     pub prg_rom: Vec<u8>,
+    pub chr_rom: Vec<u8>,
+    pub mapper: u8,
+    pub screen_mirroring: Mirroring,
 }
 
 impl Rom {
@@ -13,14 +22,38 @@ impl Rom {
         header.copy_from_slice(&raw_bytes[0..16]);
 
         if &header[0..4] != b"NES\x1a" {
-            return Err("Not valid iNES file".to_string())
+            return Err("Not valid iNES file".to_string());
         }
 
-        let prg_rom = raw_bytes[16..].to_vec();
+        let prg_rom_size = header[4] as usize * 16384;
+        let chr_rom_size = header[5] as usize * 8192;
+
+        let flags_6 = raw_bytes[6];
+        let flags_7 = raw_bytes[7];
+
+        let prg_rom_start = 16;
+        let prg_rom_end = prg_rom_start + prg_rom_size;
+
+        let chr_rom_start = prg_rom_end;
+        let chr_rom_end = chr_rom_start + chr_rom_size;
+
+        let prg_rom = raw_bytes[chr_rom_start..chr_rom_end].to_vec();
+        let chr_rom = raw_bytes[chr_rom_start..chr_rom_end].to_vec();
+
+        let mapper = (flags_7 & 0xF0) | (flags_6 >> 4);
+
+        let screen_mirroring = if (flags_6 & 1) != 0 {
+            Mirroring::Vertical
+        } else {
+            Mirroring::Horizontal
+        };
 
         Ok(Rom {
             header,
-            prg_rom
+            prg_rom,
+            chr_rom,
+            mapper,
+            screen_mirroring,
         })
     }
 }
